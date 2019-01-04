e5f473e741319e9cd15b1ae17b0e5f4b8579d9ae
Harun Richards <harunrich@googlemail.com>
2019-01-04 20:35:35 +0000

    stopped the sharp plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,7 +14,7 @@ module.exports = {
       },
     },
     `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    // `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
