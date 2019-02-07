cca3bc7810de72a5a438eb42eb019267a6ec3953
hricha05 <harunrich@googlemail.com>
2019-02-07 13:37:40 +0000

    refactored page layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,36 +1,26 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import Paper from '@material-ui/core/Paper'
-
+import { Paper, Typography, Divider } from '@material-ui/core'
 import SimpleAppBar from './appbar'
 import './layout.css'
 
 function Layout ({ children }) {
 
-  return (
-    <div>
+  return <div>
       <SimpleAppBar />
-        <div
-          style={{
-              margin: `0 auto`,
-              maxWidth: 960,
-              padding: `0px 1.0875rem 1.45rem`,
-              paddingTop: 0,
-            }}
-          >
-        <Paper  elevation={0} 
-                square={true}
-                style={{
-                  width: '100%',
-                }}>
-          {children}
-        </Paper>
-          <footer>
-            © 2018, Built with <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
-        </div>
+      <Divider component='hr' varient='fullWidth' light={true} /> 
+      <div style={{ margin: `0 auto` }}>
+        {children}
+        <Divider component='hr' light={true} variant='fullWidth' />
+        <footer>
+          <Paper square style={{ backgroundColor: '#716B7F', color: '#FFF' }}>
+            <Typography component="p">
+              © 2018, Built with <a href="https://www.gatsbyjs.org">Gatsby</a>
+            </Typography>
+          </Paper>
+        </footer>
+      </div>
     </div>
-    )
   }
 
 Layout.propTypes = {
